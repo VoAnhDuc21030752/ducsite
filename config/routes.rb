a3a2e526402020ad778fea6a9ea8971675cc0a75
unknown <ducd38435@gmail.com>
2025-01-10 15:14:16 +0700

    update 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get "products/index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
   root "products#index"
 
   get "/products", to: "products#index"
